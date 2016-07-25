310b0aff965f70a8e7e346ff67de00c227773563
br3ncha <bgonzalez148@hotmail.com>
2016-07-25 11:22:24 -0400

    "clock"

--- dab.js
+++ dab.js
@@ -4,6 +4,10 @@ $(document).ready( function() {
     var currentTime = new Date();
     var hours = currentTime.getHours();
     var minutes = currentTime.getMinutes();
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+
     var meridiem = "AM";
       if (hours > 12) {
         hours = hours - 12;
@@ -12,6 +16,10 @@ $(document).ready( function() {
 
       if (hours === 0) {
         hours = 12;
+      }
+
+      if (minutes < 10) {
+        minutes = "0" + minutes;
       }
 
     var clockDiv = document.getElementById('clock');
