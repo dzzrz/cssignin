e34f787137ce03a46f3bedafe55e6ed5172c8c65
dzarco <zarcostudent@gmail.com>
2016-07-25 11:57:15 -0400

    Added page 2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import jinja2
 import os
 import webapp2
+from google.appengine.ext import ndb
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_environment = jinja2.Environment(
@@ -12,6 +13,9 @@ class MainHandler(webapp2.RequestHandler):
         html = template.render({})
         self.response.write(html)
 
+	def post(self):
+		self.response.write(name_process(self.request.get("name")))
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler) 
 ], debug=True)
