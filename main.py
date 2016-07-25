892704073acbc34e5bd7460f22250e7ac1de3579
dzarco <zarcostudent@gmail.com>
2016-07-25 12:33:59 -0400

    Compromise of changes.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import jinja2
 import os
 import webapp2
-from google.appengine.ext import ndb
+
+#from google.appengine.ext import ndb
+
+#class Checkin1(ndb.Model):
+#    checkinName = ndb.StringProperty(required=True)
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_environment = jinja2.Environment(
@@ -16,6 +20,18 @@ class MainHandler(webapp2.RequestHandler):
 	def post(self):
 		self.response.write(name_process(self.request.get("name")))
 
+#class CheckinHandler(webapp2.RequestHandler):
+#    def get(self):
+#        self.response.write('Check in:<br>')
+#        checkin_name_query = Checkin1.query()
+#        current_check_in = checkin_name_query.fetch(limit=30)
+#        for checkinName in current_check_in:
+#            self.response.write(checkinName)
+            #self.response.write(' ')
+            #self.response.write(checkinName.checkinName)
+            #self.response.write('<br>')
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler) 
+    ('/', MainHandler)#,
+    #('/checkin', CheckinHandler),
 ], debug=True)
