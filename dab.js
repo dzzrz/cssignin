2c90de1a766efab10218786216d2f6feafa5f1a7
br3ncha <bgonzalez148@hotmail.com>
2016-07-25 12:19:35 -0400

    "notes and js changes"

--- dab.js
+++ dab.js
@@ -4,27 +4,14 @@ $(document).ready( function() {
     var currentTime = new Date();
     var hours = currentTime.getHours();
     var minutes = currentTime.getMinutes();
-      if (minutes < 10) {
-        minutes = "0" + minutes;
-      }
-
-    var meridiem = "AM";
-      if (hours > 12) {
-        hours = hours - 12;
-        meridiem = "PM";
-      }
-
-      if (hours === 0) {
-        hours = 12;
-      }
-
-      if (minutes < 10) {
-        minutes = "0" + minutes;
-      }
+    var ampm = hours >= 12 ? 'pm' : 'am';
+     hours = hours % 12;
+     hours = hours ? hours : 12; // the hour '0' should be '12'
+     minutes = minutes < 10 ? '0'+minutes : minutes;
 
     var clockDiv = document.getElementById('clock');
 
-    clockDiv.innerText = hours + ":" + minutes + " " + meridiem;
+    clockDiv.innerText = hours + ":" + minutes + " " + ampm;
 
   }
 
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import jinja2
 import os
 import webapp2
+#from google.appengine.ext import ndb
+
+#class Checkin1(ndb.Model):
+#    checkinName = ndb.StringProperty(required=True)
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_environment = jinja2.Environment(
@@ -12,6 +16,18 @@ class MainHandler(webapp2.RequestHandler):
         html = template.render({})
         self.response.write(html)
 
+#class CheckinHandler(webapp2.RequestHandler):
+#    def get(self):
+#        self.response.write('Check in:<br>')
+#        checkin_name_query = Checkin1.query()
+#        current_check_in = checkin_name_query.fetch(limit=30)
+#        for checkinName in current_check_in:
+#            self.response.write(checkinName)
+            #self.response.write(' ')
+            #self.response.write(checkinName.checkinName)
+            #self.response.write('<br>')
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler)#,
+    #('/checkin', CheckinHandler),
 ], debug=True)
