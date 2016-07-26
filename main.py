041d27e393a37e7035d1f4d25673727a6798da3a
br3ncha <bgonzalez148@hotmail.com>
2016-07-26 18:05:35 -0400

    "python changes"

--- main.py
+++ main.py
@@ -21,34 +21,47 @@ class MainHandler(webapp2.RequestHandler):
         template = template = jinja_environment.get_template('dab.html')
         self.response.out.write(template.render())
     def post(self):
+        contactinfo = '83257704342'
         name = self.request.get('name')
         location_atm = self.request.get('location_atm')
         check_in = CheckIn(name=name, location_atm=location_atm)
         check_in.put()
-        self.response.write('Check in:<br>')
+        my_checkins = {"checkin": "<tr><td> + carl + </td></tr> "}
+        table_checkin = ""
         check_in_query = CheckIn.query().order(CheckIn.time_stamp).filter(CheckIn.time_stamp >= datetime.now().replace( hour=0 ))
         check_ins = check_in_query.fetch(limit=30)
         check_ins.append(check_in)
         for check_in in check_ins:
-            self.response.write("<br>" + check_in.name + "<br>" + " - " + check_in.location_atm + "<br>" + str(check_in.time_stamp))
-
-class CheckInHandler(webapp2.RequestHandler, ndb.Model):
-    def get(self):
-        self.response.write('Check in:<br>')
-        check_in_query = CheckIn.query().order(CheckIn.time_stamp).filter(CheckIn.time_stamp >= datetime.now().replace( hour=0 ))
-        check_ins = check_in_query.fetch(limit=30)
-        for check_in in check_ins:
-            self.response.write("<br>" + check_in.name + "<br>" + " - " + check_in.location_atm + "<br>" + str(check_in.time_stamp))
+            table_checkin = table_checkin + "<tr><td>" + check_in.name + "<td>" + str(contactinfo) + "<td>" + check_in.location_atm + "<td>" + str(check_in.time_stamp) + "</td></tr>"
+        my_checkins = {"checkin": table_checkin}
+        if CheckIn.name is "Brenda":
+            print contactinfo[0]
+        else:
+            print contactinfo[1]
+        template = jinja_environment.get_template('display.html')
+        self.response.out.write(template.render(my_checkins))
 
 class MenuHandlerHome(webapp2.RequestHandler):
     def get(self):
-        template = template = jinja_environment.get_template('dab.html')
+        template = jinja_environment.get_template('dab.html')
         self.response.out.write(template.render())
 
 class MenuHandlerSignIn(webapp2.RequestHandler):
     def get(self):
-        template = template = jinja_environment.get_template('display.html')
-        self.response.out.write(template.render())
+        contactinfo = '83257704342'
+        my_checkins = {"checkin": "<tr><td> + carl + </td></tr> "}
+        table_checkin = ""
+        check_in_query = CheckIn.query().order(CheckIn.time_stamp).filter(CheckIn.time_stamp >= datetime.now().replace( hour=0 ))
+        check_ins = check_in_query.fetch(limit=30)
+        for check_in in check_ins:
+            table_checkin = table_checkin + "<tr><td>" + check_in.name + "<td>" + str(contactinfo) + "<td>" + check_in.location_atm + "<td>" + str(check_in.time_stamp) + "</td></tr>"
+        my_checkins = {"checkin": table_checkin}
+        if CheckIn.name is "Brenda":
+            print contactinfo[0]
+        else:
+            print contactinfo[1]
+        template = jinja_environment.get_template('display.html')
+        self.response.out.write(template.render(my_checkins))
 
 class MenuHandlerGoogle(webapp2.RequestHandler):
     def get(self):
@@ -57,7 +70,6 @@ class MenuHandlerGoogle(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/checkin', CheckInHandler),
     ('/dab.html', MenuHandlerHome),
     ('/display.html', MenuHandlerSignIn),
     ('/google.html', MenuHandlerGoogle)
