b85f58c36d284191715523b3a9313c0d2cb1c54f
dzarco <zarcostudent@gmail.com>
2016-07-26 09:53:59 -0400

    Update (7-26-16)

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import jinja2
 import os
 import webapp2
-
 from google.appengine.ext import ndb
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_environment = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(template_dir))
 
 class CheckIn(ndb.Model):
     name = ndb.StringProperty()
     location_atm = ndb.StringProperty()
     time_stamp = ndb.TimeProperty(auto_now_add=True)
     date_stamp = ndb.DateProperty(auto_now_add=True)
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_environment = jinja2.Environment(
-  loader=jinja2.FileSystemLoader(template_dir))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -24,14 +24,14 @@ class MainHandler(webapp2.RequestHandler):
             check_in.put()
             self.redirect('/checkin')
 
-class CheckInHandler(webapp2.RequestHandler):
+class CheckInHandler(webapp2.RequestHandler, ndb.Model):
     def get(self):
         self.response.write('Check in:<br>')
         check_in_query = CheckIn.query().order(CheckIn.time_stamp)
         check_ins = check_in_query.fetch()
         for check_in in check_ins:
             self.response.write("<br>" + check_in.name + "<br>" + " - " + check_in.location_atm + "<br>" + str(check_in.time_stamp) + " " + str(check_in.date_stamp))
-
+            
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/checkin', CheckInHandler),
