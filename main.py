d057adeabda035317d3331d85bf84407deeb9157
br3ncha <bgonzalez148@hotmail.com>
2016-07-27 10:04:17 -0400

    "hour change"

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import datetime
 import urllib2
 import json
 from datetime import datetime
+import time
 from google.appengine.ext import ndb
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
@@ -21,7 +22,6 @@ class MainHandler(webapp2.RequestHandler):
         template = template = jinja_environment.get_template('dab.html')
         self.response.out.write(template.render())
     def post(self):
-        contactinfo = '83257704342'
         name = self.request.get('name')
         location_atm = self.request.get('location_atm')
         check_in = CheckIn(name=name, location_atm=location_atm)
@@ -32,12 +32,8 @@ class MainHandler(webapp2.RequestHandler):
         check_ins = check_in_query.fetch(limit=30)
         check_ins.append(check_in)
         for check_in in check_ins:
-            table_checkin = table_checkin + "<tr><td>" + check_in.name + "<td>" + str(contactinfo) + "<td>" + check_in.location_atm + "<td>" + str(check_in.time_stamp) + "</td></tr>"
+            table_checkin = table_checkin + "<tr><td>" + check_in.name + "<td>" + "unavailable" + "<td>" + check_in.location_atm + "<td>" + str(check_in.time_stamp) + "</td></tr>"
         my_checkins = {"checkin": table_checkin}
-        if CheckIn.name is "Brenda":
-            print contactinfo[0]
-        else:
-            print contactinfo[1]
         template = jinja_environment.get_template('display.html')
         self.response.out.write(template.render(my_checkins))
 
@@ -48,18 +44,13 @@ class MenuHandlerHome(webapp2.RequestHandler):
 
 class MenuHandlerSignIn(webapp2.RequestHandler):
     def get(self):
-        contactinfo = '83257704342'
         my_checkins = {"checkin": "<tr><td> + carl + </td></tr> "}
         table_checkin = ""
-        check_in_query = CheckIn.query().order(CheckIn.time_stamp).filter(CheckIn.time_stamp >= datetime.now().replace( hour=0 ))
+        check_in_query = CheckIn.query().order(CheckIn.time_stamp).filter(CheckIn.time_stamp >= datetime.now().replace( hour=4 ))
         check_ins = check_in_query.fetch(limit=30)
         for check_in in check_ins:
-            table_checkin = table_checkin + "<tr><td>" + check_in.name + "<td>" + str(contactinfo) + "<td>" + check_in.location_atm + "<td>" + str(check_in.time_stamp) + "</td></tr>"
+            table_checkin = table_checkin + "<tr><td>" + check_in.name + "<td>" + "unavailable" + "<td>" + check_in.location_atm + "<td>" + str(check_in.time_stamp) + "</td></tr>"
         my_checkins = {"checkin": table_checkin}
-        if CheckIn.name is "Brenda":
-            print contactinfo[0]
-        else:
-            print contactinfo[1]
         template = jinja_environment.get_template('display.html')
         self.response.out.write(template.render(my_checkins))
 
